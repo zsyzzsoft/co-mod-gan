b006bdcb6e1fc96649c1d6fee5099ee3d6f38f5c
Shengyu Zhao <zsyzzsoft@gmail.com>
2021-06-09 23:47:35 +0800

    Added truncation option

--- run_generator.py
+++ run_generator.py
@@ -5,7 +5,7 @@ import PIL.Image
 from dnnlib import tflib
 from training import misc
 
-def create_from_images(checkpoint, image, mask, output):
+def create_from_images(checkpoint, image, mask, output, truncation):
     real = np.asarray(PIL.Image.open(image)).transpose([2, 0, 1])
     real = misc.adjust_dynamic_range(real, [0, 255], [-1, 1])
     mask = np.asarray(PIL.Image.open(mask).convert('1'), dtype=np.float32)[np.newaxis]
@@ -13,7 +13,7 @@ def create_from_images(checkpoint, image, mask, output):
     tflib.init_tf()
     _, _, Gs = misc.load_pkl(checkpoint)
     latent = np.random.randn(1, *Gs.input_shape[1:])
-    fake = Gs.run(latent, None, real[np.newaxis], mask[np.newaxis])[0]
+    fake = Gs.run(latent, None, real[np.newaxis], mask[np.newaxis], truncation_psi=truncation)[0]
     fake = misc.adjust_dynamic_range(fake, [-1, 1], [0, 255])
     fake = fake.clip(0, 255).astype(np.uint8).transpose([1, 2, 0])
     fake = PIL.Image.fromarray(fake)
@@ -25,6 +25,7 @@ def main():
     parser.add_argument('-i', '--image', help='Original image path', required=True)
     parser.add_argument('-m', '--mask', help='Mask path', required=True)
     parser.add_argument('-o', '--output', help='Output (inpainted) image path', required=True)
+    parser.add_argument('-t', '--truncation', help='Truncation psi for the trade-off between quality and diversity. Defaults to 1.', default=None)
 
     args = parser.parse_args()
     create_from_images(**vars(args))
